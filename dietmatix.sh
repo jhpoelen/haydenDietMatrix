8538f42231c37043e509745dab96d297d2c47646
jhpoelen <jhpoelen@xs4all.nl>
2018-06-27 20:01:33 -0700

    add predprey order prey frequency table to troubleshoot prey mappings @BrianHayden

--- dietmatix.sh
+++ dietmatix.sh
@@ -6,4 +6,4 @@ zcat interactionsPredPrey.tsv.gz | nomer append | grep SAME_AS | cut -f3,4,6,7 |
 nomer properties | grep -v "nomer.append.schema.output" > my.properties
 echo 'nomer.append.schema.output=[{"column":0,"type":"path.order.id"},{"column": 1,"type":"path.order.name"},{"column": 2,"type":"path.order"}]' >> my.properties
 zcat interactionsPreyPred.tsv.gz | nomer append --properties=my.properties | grep SAME_AS | awk -F '\t' '{ print $3 "\t" $4 "\t" $1 "\t" $2 "\t" $6 "\t" $7}' | grep -P "^FBC:FB" | grep -v -P "\t\t$" | gzip > fbPredPreyOrder.tsv.gz
-
+zcat fbPredPreyOrder.tsv.gz | cut -f4,6 | sort | uniq -c | sort -n -r > fbPredPreyOrderPreyFrequency.tsv

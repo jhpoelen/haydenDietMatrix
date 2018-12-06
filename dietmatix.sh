513953de7b4abe41d83bc85adc6c77f0625adaaa
jhpoelen <jhpoelen@xs4all.nl>
2018-12-06 09:35:14 -0800

    enable logging; print versions

--- dietmatix.sh
+++ dietmatix.sh
@@ -4,6 +4,12 @@
 # and apache spark are available via elton, nomer, and spark-shell aliases respectively.
 #
 
+set -xe 
+
+elton version
+nomer version
+spark-shell --version
+
 elton interactions | cut -f1,2,8,9,10 | sort | uniq | gzip > interactions.tsv.gz
 zcat interactions.tsv.gz | cut -f3 | sort | uniq > interactionLabel.tsv
 zcat interactions.tsv.gz | grep -P "(\teatenBy\t|\tpreyedUponBy\t)" | awk -F '\t' '{ print $4 "\t" $5 "\t" $1 "\t" $2 }' | gzip > interactionsPredPrey.tsv.gz 

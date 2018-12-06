f6f4326e968db25db14ee3db2a617389aa390e15
jhpoelen <jhpoelen@xs4all.nl>
2018-12-06 09:22:49 -0800

    make predator and prey name matching properties explicit

--- dietmatix.sh
+++ dietmatix.sh
@@ -8,12 +8,12 @@ elton interactions | cut -f1,2,8,9,10 | sort | uniq | gzip > interactions.tsv.gz
 zcat interactions.tsv.gz | cut -f3 | sort | uniq > interactionLabel.tsv
 zcat interactions.tsv.gz | grep -P "(\teatenBy\t|\tpreyedUponBy\t)" | awk -F '\t' '{ print $4 "\t" $5 "\t" $1 "\t" $2 }' | gzip > interactionsPredPrey.tsv.gz 
 zcat interactions.tsv.gz | grep -P "(\teats\t|\tpreysOn\t)" | cut -f1,2,4,5 | gzip >> interactionsPredPrey.tsv.gz
-zcat interactionsPredPrey.tsv.gz | nomer append | grep SAME_AS | cut -f3,4,6,7 | gzip > interactionsPreyPred.tsv.gz
+
+# resolve predator names 
+zcat interactionsPredPrey.tsv.gz | nomer append --properties=predator.properties | grep SAME_AS | cut -f3,4,6,7 | gzip > interactionsPreyPred.tsv.gz
 
 # map to prey orders
-nomer properties | grep -v "nomer.append.schema.output" > my.properties
-echo 'nomer.append.schema.output=[{"column":0, "type":"id"}, {"column":1, "type":"name"}, {"column": 2, "type": "rank"}, {"column":3,"type":"path.order.id"},{"column":4,"type":"path.order.name"},{"column":5,"type":"path.order"}]' >> my.properties
-zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=my.properties | grep SAME_AS | gzip > fbPreyPredSameAsWithOrder.tsv.gz
+zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyOrder.properties | grep SAME_AS | gzip > fbPreyPredSameAsWithOrder.tsv.gz
 
 # remove likely homonyms
 zcat fbPreyPredSameAsWithOrder.tsv.gz | awk -F '\t' '{ print $1 "\t" $2 "\t" $6 "\t" $7 }' | sort | uniq | gzip > fbPreyMap.tsv.gz

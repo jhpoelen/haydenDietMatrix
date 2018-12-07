77393e7c6c1e4286fb30befe52b94ef68d3b28a5
jhpoelen <jhpoelen@xs4all.nl>
2018-12-07 14:32:40 -0800

    update README and script

--- dietmatrix.sh
+++ dietmatrix.sh
@@ -7,14 +7,17 @@
 set -xe 
 
 date --iso-8601=seconds
+uname -a
+java -version
 elton version
 nomer version
+spark-shell --version
 
 # download the dataset cache (>20GB uncompressed)
 curl -L https://zenodo.org/record/2007419/files/elton-datasets.tar.gz | tar xfz - 
 
 elton interactions | cut -f2,3,13,15,16 | gzip > interactions_dups.tsv.gz
-zcat interactions_dups.tsv.gz | sort | uniq > interactions.tsv.gz
+zcat interactions_dups.tsv.gz | sort | uniq | gzip > interactions.tsv.gz
 zcat interactions.tsv.gz | cut -f3 | sort | uniq > interactionLabel.tsv
 zcat interactions.tsv.gz | grep -P "(\teatenBy\t|\tpreyedUponBy\t)" | awk -F '\t' '{ print $4 "\t" $5 "\t" $1 "\t" $2 }' | gzip > interactionsPredPrey.tsv.gz 
 zcat interactions.tsv.gz | grep -P "(\teats\t|\tpreysOn\t)" | cut -f1,2,4,5 | gzip >> interactionsPredPrey.tsv.gz
@@ -24,11 +27,11 @@ zcat interactionsPredPrey.tsv.gz | nomer append --properties=predator.properties
 
 # map to prey orders
 zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyOrder.properties | grep SAME_AS | gzip > fbPreyPredSameAsWithOrder.tsv.gz
+zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyOrder.properties | grep -v SAME_AS | gzip > fbPreyPredNotSameAsWithOrder.tsv.gz
 
 # remove likely homonyms
 zcat fbPreyPredSameAsWithOrder.tsv.gz | awk -F '\t' '{ print $1 "\t" $2 "\t" $6 "\t" $7 }' | sort | uniq | gzip > fbPreyMap.tsv.gz
 
-spark-shell --version
 
 cat removeLikelyHomonyms.scala | spark-shell
 cat fbPreyLikelyHomonyms/*.csv | sort | uniq > fbPreyLikelyHomonyms.tsv

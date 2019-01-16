01946e1c16bf555341314ef38dc7a0c88991b6d6
jhpoelen <jhpoelen@xs4all.nl>
2019-01-16 11:55:37 -0800

    add taxon ids for prey hierarchy analysis following @BrianHayden observations

--- dietmatrix.sh
+++ dietmatrix.sh
@@ -65,7 +65,7 @@ function map_prey_to_rank() {
 }
 
 function map_prey_to_path() {
-  zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyPath.properties | grep SAME_AS | cut -f3,4,6,7 | gzip > fbPredPreySameAsWithFullHierarchy.tsv.gz
+  zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyPath.properties | grep SAME_AS | cut -f3,4,6,7,8 | gzip > fbPredPreySameAsWithFullHierarchy.tsv.gz
   zcat interactionsPreyPred.tsv.gz | grep -P ".*\t.*\tFBC:FB" | nomer append --properties=preyPath.properties | grep -v SAME_AS | cut -f1,2,3,4 | gzip > fbPreyPredNotSameAsWithFullHierarchy.tsv.gz
 }
 
